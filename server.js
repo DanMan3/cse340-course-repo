fea9f23b0698c841c1cdbf5ed5c4a617a024aeca
Daniel Bennion <dhbennion@gmail.com>
2026-05-07 05:49:39 +0000

    W01 Assignment: Initial Site Creation

--- server.js
+++ server.js
@@ -45,6 +45,10 @@ app.get('/projects', async (req, res) => {
     const title = 'Service Projects';
     res.render('projects', { title });
 });
+app.get('/categories', async (req, res) => {
+    const title = 'Categories';
+    res.render('categories', { title });
+});
 
 
 app.listen(PORT, () => {
